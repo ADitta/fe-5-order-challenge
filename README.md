9a7f25380011968727c7b6a85da3d382069f8786
= <=>
2022-03-22 15:16:09 +0000

    add error handling

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 # Front-End
 
-- **Front-End - Hosted link:**
+- **Front-End - Hosted link:** https://fe-mentor-interactive-challenge.netlify.app/
 
 # Description
 
--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -1,20 +1,29 @@
 import phone from "../images/illustration-thank-you.svg";
-
+import { Link } from "react-router-dom";
 const ThankYou = ({ rating }) => {
   return (
-    <div className=" thank-you-container">
-      <img src={phone} alt="" />
-      <div className="final-rating">
-        <div className="rating-text">
-          <h2>You selected {rating} out of 5</h2>
+    <>
+      {rating ? (
+        <div className=" thank-you-container">
+          <img src={phone} alt="" />
+          <div className="final-rating">
+            <div className="rating-text">
+              <h2>You selected {rating ? rating : "?"} out of 5</h2>
+            </div>
+          </div>
+          <h1>Thank you!</h1>
+          <p>
+            We appreciate you taking the time to give a rating. If you ever need
+            more support, don't hesistate to get in touch!
+          </p>
         </div>
-      </div>
-      <h1>Thank you!</h1>
-      <p>
-        We appreciate you taking the time to give a rating. If you ever need
-        more support, don't hesistate to get in touch!
-      </p>
-    </div>
+      ) : (
+        <div className="error-content">
+          <h1>Sorry there seemed to be an error</h1>
+          <Link to="/">Click here to return back home</Link>
+        </div>
+      )}
+    </>
   );
 };
 
